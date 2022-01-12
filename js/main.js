6c8c7eb1f4ebb2252ccba08d00d008b714cc107d
hongsdf <76565908+hongsdf@users.noreply.github.com>
2022-01-12 14:58:30 +0900

    login folder

--- js/main.js
+++ js/main.js
@@ -1,100 +1,71 @@
-;(function () {
-	
-	'use strict';
 
-	// iPad and iPod detection	
-	var isiPad = function(){
-		return (navigator.platform.indexOf("iPad") != -1);
-	};
-
-	var isiPhone = function(){
-	    return (
-			(navigator.platform.indexOf("iPhone") != -1) || 
-			(navigator.platform.indexOf("iPod") != -1)
-	    );
-	};
-
-	// OffCanvass
-	var offCanvass = function() {
-		$('body').on('click', '.js-fh5co-menu-btn, .js-fh5co-offcanvass-close', function(){
-			$('#fh5co-offcanvass').toggleClass('fh5co-awake');
-		});
-	};
-
-	// Click outside of offcanvass
-	var mobileMenuOutsideClick = function() {
-		$(document).click(function (e) {
-	    var container = $("#fh5co-offcanvass, .js-fh5co-menu-btn");
-	    if (!container.is(e.target) && container.has(e.target).length === 0) {
-	    	if ( $('#fh5co-offcanvass').hasClass('fh5co-awake') ) {
-	    		$('#fh5co-offcanvass').removeClass('fh5co-awake');
-	    	}
-	    }
-		});
-
-		$(window).scroll(function(){
-			if ( $(window).scrollTop() > 500 ) {
-				if ( $('#fh5co-offcanvass').hasClass('fh5co-awake') ) {
-		    		$('#fh5co-offcanvass').removeClass('fh5co-awake');
-		    	}
-	    	}
-		});
-	};
-
-	// Magnific Popup
-	
-	var magnifPopup = function() {
-		$('.image-popup').magnificPopup({
-			type: 'image',
-			removalDelay: 300,
-			mainClass: 'mfp-with-zoom',
-			titleSrc: 'title',
-			gallery:{
-				enabled:true
-			},
-			zoom: {
-				enabled: true, // By default it's false, so don't forget to enable it
-
-				duration: 300, // duration of the effect, in milliseconds
-				easing: 'ease-in-out', // CSS transition easing function
-
-				// The "opener" function should return the element from which popup will be zoomed in
-				// and to which popup will be scaled down
-				// By defailt it looks for an image tag:
-				opener: function(openerElement) {
-				// openerElement is the element on which popup was initialized, in this case its <a> tag
-				// you don't need to add "opener" option if this code matches your needs, it's defailt one.
-				return openerElement.is('img') ? openerElement : openerElement.find('img');
-				}
-			}
-		});
-	};
+(function ($) {
+    "use strict";
 
 
-	var animateBoxWayPoint = function() {
+     /*==================================================================
+    [ Focus input ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+  
+  
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
 
-		if ($('.animate-box').length > 0) {
-			$('.animate-box').waypoint( function( direction ) {
+    $('.validate-form').on('submit',function(){
+        var check = true;
 
-				if( direction === 'down' && !$(this).hasClass('animated') ) {
-					$(this.element).addClass('bounceIn animated');
-				}
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
 
-			} , { offset: '75%' } );
-		}
-
-	};
-
-	
-
-	
-	$(function(){
-		magnifPopup();
-		offCanvass();
-		mobileMenuOutsideClick();
-		animateBoxWayPoint();
-	});
+        return check;
+    });
 
 
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
 
-}());
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
